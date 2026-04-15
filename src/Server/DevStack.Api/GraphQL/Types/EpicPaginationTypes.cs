8234b0267d9775a0a0e276950fd0102f202b2122
Martin Kuhne <9038432+MartinKuhne@users.noreply.github.com>
2026-04-15 12:05:58 -0700

    Add Epic GraphQL type definitions
    - EpicType.cs - GraphQL type with id, title, description, features
    - EpicPaginationTypes.cs - Pagination support for Epic queries

---
+++ src/Server/DevStack.Api/GraphQL/Types/EpicPaginationTypes.cs
@@ -0,0 +1,23 @@
+using DevStack.Domain.Entities;
+using HotChocolate.Types;
+
+namespace DevStack.Api.GraphQL.Types;
+
+public class EpicConnection
+{
+    public List<Epic> Nodes { get; set; } = default!;
+    public EpicPageInfo PageInfo { get; set; } = default!;
+    public int TotalCount { get; set; }
+}
+
+public class EpicPageInfo
+{
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage { get; set; }
+    public int TotalCount { get; set; }
+}
+
+public class EpicFilterInput
+{
+    public string? Title { get; set; }
+}
